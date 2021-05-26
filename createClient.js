04ae1e7f6cfc2c6da1a785929473100af9c44534
eckyecky <765268+eckyecky@users.noreply.github.com>
2021-05-26 15:22:40 +1000

    update packages

--- createClient.js
+++ createClient.js
@@ -4,14 +4,22 @@ const client = new WstClient()
 client.verbose()
 const url = require('url')
 
+function getBindParts(bindAddress) {
+  console.log(bindAddress, typeof bindAddress)
+  if (Number.isInteger(bindAddress) || bindAddress.indexOf(':') < 0)
+    return ['127.0.0.1', bindAddress]
+  const parts = bind.split(':')
+  const port = parts.pop()
+  const localAddress = parts[0] || '127.0.0.1'
+  return [localAddress, port]
+}
+
 module.exports = (address, bind, auth) => {
   const wsUrl = new url.URL(address)
   if (auth) {
     wsUrl.username = auth.username
     wsUrl.password = auth.password
   }
-  const parts = bind.split(':')
-  const port = parts.pop()
-  const localAddress = parts[0] || '127.0.0.1'
+  const [localAddress, port] = getBindParts(bind)
   client.start(localAddress, port, wsUrl.toString(), null, {})
 }
